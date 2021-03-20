50ed18ee6e462d22881b8ea41c39d72e1e47f2e8
damilolawumi <damilola.olawumi@sterling.ng>
2021-03-20 14:54:40 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import {Table} from 'antd';
 import './App.css';
 import NestedTable from "./NestedTable";
 
 
 function App() {
 
-  return (
-    <div className="App">
-      {/*WELCOME TO THE REACT PROJECT*/}
-        <NestedTable />
-    </div>
-  );
+    return (
+        <div className="App">
+            <p> WELCOME TO THE VENDOR APPLICATION </p>
+            <NestedTable/>
+        </div>
+    );
 }
 
 export default App;
--- src/NestedTable.js
+++ src/NestedTable.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Menu, Table} from 'antd';
+import {Table} from 'antd';
 import axios from 'axios';
 
 function NestedTable() {
