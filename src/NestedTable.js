f8cbaf2aac2bc275914594ac81576663b2d420c8
damilolawumi <damilola.olawumi@sterling.ng>
2021-03-20 21:17:41 +0100

    scroll view and add column added

--- src/NestedTable.js
+++ src/NestedTable.js
@@ -1,10 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import {Table} from 'antd';
+import {Table, Button} from 'antd';
 import axios from 'axios';
 
 function NestedTable() {
 
     const [vendors, setVendors] = useState([]);
+    const [columns, setColumns] = useState([
+        {title: 'Name', dataIndex: 'name', key: 'name', fixed: 'left'},
+        {title: '# of Applications', dataIndex: 'platform', key: 'platform', responsive: ['sm'], render: (text, record, index)=> {
+                return <p key={index}>{record?.applications?.length} Apps</p>
+            }},
+        {title: 'Total Spend', dataIndex: 'total_spend', key: 'total_spend'},
+        {title: 'Active Contract', dataIndex: 'active_contract', key: 'active_contract', responsive: ['md']},
+        {title: 'Source', dataIndex: 'source', key: 'source'},
+        {title: 'Contract Value', dataIndex: 'contract_value', key: 'contract_value',   responsive: ['lg']},
+    ]);
     const [loading, setLoading] = useState(false);
 
     const fetchVendorData = () => {
@@ -43,23 +53,26 @@ function NestedTable() {
         return <Table columns={columns} dataSource={data} pagination={false}/>;
     };
 
-    const columns = [
-        {title: 'Name', dataIndex: 'name', key: 'name'},
-        {title: '# of Applications', dataIndex: 'platform', key: 'platform', responsive: ['sm'], render: (text, record, index)=> {
-            return <p key={index}>{record?.applications?.length} Apps</p>
-            }},
-        {title: 'Total Spend', dataIndex: 'total_spend', key: 'total_spend'},
-        {title: 'Active Contract', dataIndex: 'active_contract', key: 'active_contract', responsive: ['md']},
-        {title: 'Source', dataIndex: 'source', key: 'source'},
-        {title: 'Contract Value', dataIndex: 'contract_value', key: 'contract_value',   responsive: ['lg']},
-    ];
+
+    const handleAddColumn = () => {
+        const oldColumns = [...columns]
+        const newColumn = {
+            title: 'Total Spend',
+            dataIndex: 'total_spend',
+        };
+        setColumns([...oldColumns, newColumn])
+    }
 
 
     return (
         <div className='nested-container'>
+            <Button onClick={handleAddColumn} type="primary" style={{ marginBottom: 16 }}>
+                Add a column
+            </Button>
             <Table
                 loading={loading}
                 bordered
+                scroll={{ x: 1300 }}
                 className="components-table-demo-nested"
                 columns={columns}
                 expandable={{expandedRowRender}}
